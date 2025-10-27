1f6b47a9e030d7c513140888547a5e42e6ac2f76
JavierC1991 <martinezolban2015@gmail.com>
2025-10-27 04:27:45 -0600

    se agregaron validaciones al DTO create-pokemon.dto

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,16 @@
-export class CreatePokemonDto {}
+import { IsInt, IsPositive, IsString, Min, MinLength } from 'class-validator';
+
+export class CreatePokemonDto {
+  @IsPositive({ message: `el valor para el campo 'no' debe ser positivo` })
+  @IsInt({ message: `el valor para el campo 'no' no es un numero valido` })
+  @Min(1, { message: `el valor minimo para el campo 'no' es 1` })
+  no: number;
+
+  @IsString({
+    message: `El valor proporcionado para el campo 'name' no es valido`,
+  })
+  @MinLength(1, {
+    message: `El tamño minimo de caracteres para el campo 'name' es de 1`,
+  })
+  name: string;
+}
